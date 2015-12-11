7f0820142f6c38435cb008f0b6bef4b1f85c6240
totutotu <totutotu@cs.helsinki.fi>
2015-12-11 13:33:23 +0200

    eka

--- routes/topics.js
+++ routes/topics.js
@@ -9,22 +9,37 @@ var Models = require('../models');
 // GET /topics
 router.get('/', function(req, res, next) {
     // Hae kaikki aihealueet tässä (Vinkki: findAll)
-    res.send(200);
+    
+    Models.Topic.findAll().then(function(topics){
+    console.log("oispa logi");
+        res.json(topics);
+//        res.sendStatus(200);
+
+    });
+
 });
 
 // GET /topics/:id
 router.get('/:id', function(req, res, next) {
-  // Hae aihealue tällä id:llä tässä (Vinkki: findOne)
-  var topicId = req.params.id;
-  res.send(200);
+
+    var topicId = req.params.id;
+    Models.Topic.findOne({where: {id: topicId}}).then(function(topic){
+        res.json(topic);
+//        res.sendStatus(200);
+
+    });
 });
 
 // POST /topics
 router.post('/', function(req, res, next) {
   // Lisää tämä aihealue
   var topicToAdd = req.body;
+  
+  Models.Topic.create(topicToAdd).then(function(topic){
+      res.json(topic);
+  
+  });
   // Palauta vastauksena lisätty aihealue
-  res.send(200);
 });
 
 // POST /topics/:id/message
@@ -34,7 +49,7 @@ router.post('/:id/message', function(req, res, next) {
   // ...tämä viesti (Vinkki: lisää ensin messageToAdd-objektiin kenttä TopicId, jonka arvo on topicId-muuttujan arvo ja käytä sen jälkeen create-funktiota)
   var messageToAdd = req.body;
   // Palauta vastauksena lisätty viesti
-  res.send(200);
+  res.sendStatus(200);
 });
 
 module.exports = router;
